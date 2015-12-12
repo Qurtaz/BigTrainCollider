b8b08d8035c699d219454f87d4e07237c9fba68d
Qurtaz <piwowino1994@gamil.com>
2015-12-12 14:18:49 +0100

    create structure and Train

--- Assets/Script/Ruch.cs
+++ Assets/Script/Ruch.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Control : MonoBehaviour {
+public class Ruch : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Script/Train.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script
+{
+    class Train : MonoBehaviour
+    {
+        private int howManyCar;
+        public GameObject car1;
+        public GameObject car2;
+        public GameObject car3;
+        public GameObject car4;
+        public Transform locomotive;
+        public void addCar()
+        {
+        howManyCar=howManyCar+1;
+        }
+        public void deleteCar(int N)
+        {
+            howManyCar = howManyCar - N;
+        }
+        public void createAllCar()
+        {
+            Vector3 przesun = new Vector3(0.2f, 0f, 0f);
+            for(int i=0;i<=howManyCar;i++)
+            {
+                Instantiate(car1, locomotive.position-(przesun* i), locomotive.rotation);
+            }
+        }
+    }
+}
